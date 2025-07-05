20d79c1ec694845656bcbfbac4d0515f09e044c3
prd-demo-admin <u21231696@utp.edu.pe>
2025-07-04 21:24:54 -0500

    login patient

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,7 +8,8 @@ INSERT INTO auth_role (name) VALUES ('ROLE_ADMIN'),('ROLE_DOCTOR'),('ROLE_RECEPT
 -- USERS
 -- ============================================
 INSERT INTO auth_user (username, password, account_locked, enabled, role_id) VALUES ('admin', '$2a$10$NRFP84zXJobHNEr1xlRzhOYb4lkY4ukRcG3oNMkexJAhqSGnX3pbu', 0, 1, 1),('recep', '$2a$10$GcXjFyWNnAGq4/wkiCQ2DO79c3bmeeqPo78zZmbRWSUK9mfQ46U5S', 0, 1, 2);
-
+-- patient
+INSERT INTO auth_user (username, password, account_locked, enabled, role_id) VALUES ('patient', '$2a$10$NRFP84zXJobHNEr1xlRzhOYb4lkY4ukRcG3oNMkexJAhqSGnX3pbu', 0, 1, 4);
 -- ============================================
 -- OAUTH CLIENT: citas_app
 -- ============================================
@@ -38,6 +39,22 @@ INSERT INTO client_redirect_uris (client_id, redirect_uri) VALUES (2, 'http://lo
 INSERT INTO client_logout_redirect_uri (client_id, logout_redirect_uri) VALUES (2, 'http://localhost:5173/authorized?logout=logout_action');
 
 INSERT INTO client_scope (client_id, scope) VALUES (2, 'openid');
+
+
+-- ============================================
+-- OAUTH CLIENT: patient app
+-- ============================================
+INSERT INTO client_app (client_id, client_secret, client_name, duration_in_minutes, require_proof_key) VALUES ('patientApp','$2a$10$NRFP84zXJobHNEr1xlRzhOYb4lkY4ukRcG3oNMkexJAhqSGnX3pbu','Patient app',36000,0);
+
+INSERT INTO client_authentication_methods (client_id, authentication_method) VALUES (3, 'client_secret_basic');
+
+INSERT INTO client_authorization_grant_type (client_id, authorization_grant_type) VALUES(3, 'authorization_code'),(3, 'refresh_token'),(3, 'client_credentials');
+
+INSERT INTO client_redirect_uris (client_id, redirect_uri) VALUES (3, 'http://localhost:3000');
+
+INSERT INTO client_logout_redirect_uri (client_id, logout_redirect_uri) VALUES (3, 'http://localhost:3000/authorized?logout=logout_action');
+
+INSERT INTO client_scope (client_id, scope) VALUES (3, 'openid');
 
 -- ============================================
 -- SCOPES COMENTADOS (opcional)
